15e869515e457a21fe25765d022aa8d34f5eaeee
agostinopique <ago.pique@yahoo.it>
2022-04-29 17:25:57 +0200

    Milestone 1;

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-[
+const iconList = [
 	{
 		name: 'cat',
 		prefix: 'fa-',
@@ -112,3 +112,56 @@
 		color: 'blue'
 	}
 ];
+
+/* 
+	**Milestone 1**
+	Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell’icona e l’icona stessa.
+
+	**Milestone 2**
+	Ciascuna icona ha una proprietà “color”: utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
+
+	**Milestone 3**
+	Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone *(animal, vegetable, user)*. Quando l’utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
+
+	**BONUS**
+	1- modificare la struttura dati fornita e valorizzare la proprietà “color” in modo dinamico: generare in modo casuale un codice colore, sapendo che la notazione esadecimale è formata dal simbolo “#” seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
+	2- popolare le options della select della milestone 3 dinamicamente.
+
+
+	MILESTONE 1:
+	1. Crea una funzione che va a creare dei box con le classi boostrap e non che andranno poi stampate all'interno della row di bootstrap;
+	2. Creare una funzione che va a stampare il nome e il codice dell'icona all'interno del box creato al punto sopra.
+
+	MILESTONE 2:
+	1. Creare una funzione che va a leggere la proprietá color di ogni oggetto e ad aggiungerla come style all'icona corrispondente;
+
+	MILESTONE 3:
+	1. Creare una funzione con il quale vado a leggere il value dell'option selezionato dell'user.
+	2. Stampare a schermo solo le icone con il type corrispondente.
+*/
+
+const iconSection = document.getElementById('icon_section');
+
+iconBox()
+
+function iconBox(){
+	
+iconList.forEach((icon) => {
+	const col = document.createElement('div');
+
+	col.className = 'box';
+	col.classList.add('m-3', 'text-center');
+	
+	iconSection.append(col);
+	
+	const {name, family, prefix} = icon;
+	console.log(icon)
+	
+	col.innerHTML = `
+		<i class='${family} ${prefix}${name} fa-2x'></i>
+		<span>${name}</span>`
+})
+
+
+	
+}
