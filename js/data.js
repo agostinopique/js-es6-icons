c7881600fd03cbbb6878952a070cd4a0dd0a4072
agostinopique <ago.pique@yahoo.it>
2022-04-29 19:06:35 +0200

    Milestone 3; Creata meccanica di stampa a seconda del valore selezionato dall'user;

--- js/data.js
+++ js/data.js
@@ -137,29 +137,79 @@ const iconList = [
 
 	MILESTONE 3:
 	1. Creare una funzione con il quale vado a leggere il value dell'option selezionato dell'user.
-	2. Stampare a schermo solo le icone con il type corrispondente.
+	2. Creo delle funzioni che filtrano gli oggetti in base al type;
+	3. Stampo a schermo l'array che corrisponde al type selezionato dall'utente;
 */
 
 const iconSection = document.getElementById('icon_section');
 
-iconBox()
 
-function iconBox(){
-	
-iconList.forEach((icon) => {
-	const col = document.createElement('div');
 
-	col.className = 'box';
-	col.classList.add('m-3', 'text-center');
+const animalArray = iconList.filter((icon) => icon.type === 'animal');
+const vegetableArray = iconList.filter((icon) => icon.type === 'vegetable');
+const userArray = iconList.filter((icon) => icon.type === 'user');
+
+// console.log(animalArray)
+// console.log(vegetableArray)
+// console.log(userArray)
+
+iconBox(iconList);
+
+document.getElementById('select-type').addEventListener('change', function(){
+	switch (this.value) {
+		case 'animal':
+			iconSection.innerHTML = '';
+			iconBox(animalArray);
+			break;
+
+		case 'user':
+			iconSection.innerHTML = '';
+			iconBox(userArray);
+			break;
+
+		case 'vegetable':
+			iconSection.innerHTML = '';
+			iconBox(vegetableArray);
+			break;
+
+		case 'all':
+			iconSection.innerHTML = '';
+			iconBox(iconList);
+			break;
+				
+		default:
+			break;
+	}
+});
+
+
+
+function iconBox(array){
 	
-	iconSection.append(col);
-	
-	const {name, family, prefix, color} = icon;
-	console.log(icon)
-	
-	col.innerHTML = `
-		<i class='${family} ${prefix}${name} fa-2x ${color}'></i>
-		<span>${name}</span>`
-})
+	array.forEach((icon) => {
+		const cell = document.createElement('div');
+
+		cell.className = 'box';
+		cell.classList.add('m-3', 'text-center');
+		
+		iconSection.append(cell);
+		
+		const {name, family, prefix, color} = icon;
+		// console.log(icon)
+		
+		cell.innerHTML = `
+			<i class='${family} ${prefix}${name} fa-2x ${color}'></i>
+			<span>${name}</span>`
+	})
 
 }
+
+/* 
+
+	**BONUS**
+	1- modificare la struttura dati fornita e valorizzare la proprietà “color” in modo dinamico: generare in modo casuale un codice colore, sapendo che la notazione esadecimale è formata dal simbolo “#” seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
+	2- popolare le options della select della milestone 3 dinamicamente.
+
+
+	1. Crea una funzione che estrae 
+*/
